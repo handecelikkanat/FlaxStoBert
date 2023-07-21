0d98a844824134f2174bb11529f21b6592bb231c
Hande <7702228+handecelikkanat@users.noreply.github.com>
2023-07-21 16:47:28 +0300

    added runner code

---
+++ run_jax_stobert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=train_stobert_nli_chaosnli
+#SBATCH --account=Project_2001194
+#SBATCH -o stobert_chaosnli_res.txt
+#SBATCH --partition=gpu
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=8000
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=elaine.zosa@helsinki.fi
+
+source /home/hande/Work/bert_jax/venv/bin/activate
+
+BASE=/home/hande/Work/
+
+python $BASE/bert_jax/train_jax_stobert.py \
+	--dataset mnli-m-small-chaosnli \
+	--data_path cvbert_data/NLI \
+	--model_name_or_path bert-base-uncased \
+	--train_batch_size 16 \
+	--eval_batch_size 16 \
+	--num_train_epochs 10 \
+	--learning_rate 2e-5 \
+	--output_dir $BASE/outputs/bert_jax
+
